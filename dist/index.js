48c31322f3e209d2c62916e1e3bc71a174a86f36
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-11-04 07:45:06 -0300

    0.1.19

--- dist/index.js
+++ dist/index.js
@@ -9738,12 +9738,13 @@ const exec = __nccwpck_require__(2049);
 const github = __nccwpck_require__(3134);
 let fail = true;
 let pr_message = true;
+let GITHUB_TOKEN = '';
 let output = '';
 const default_no_error_message = 'No lint errors found';
 
-function commentPr(message) {
+function commentPr(message, token) {
     const context = github.context;
-    const client = github.getOctokit(process.env.GITHUB_TOKEN);
+    const client = github.getOctokit(token);
     client.issues.createComment({
         ...context.repo,
         issue_number: context.payload.pull_request.number,
@@ -9789,6 +9790,7 @@ async function run() {
         const path = core.getInput('path');
         fail = core.getInput('fail');
         pr_message = core.getInput('pr-message');
+        GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
         // Install pylint
         await exec.exec('pip', ['install', 'pylint']);
@@ -9819,7 +9821,7 @@ async function run() {
 
         // Comment on PR
         if ((pr_message) && (message !== default_no_error_message)) {
-            commentPr(message);
+            commentPr(message, GITHUB_TOKEN);
         }
 
         // Fail if needed
